5c043a812908449c8647c22d182fe1d324dfd060
Christiano Haesbaert <haesbaert@haesbaert.org>
2016-08-16 22:45:07 +0200

    Parse version banner from clients.
    RFCs is a bit permissive here, it allows custom lines to be sent before the SSH-
    line, it also requires that SSH- starts in a new line.
    
    I've tried to be very careful and consider every case.
    
    I'm still unsure on how to do the stream processing in a side effect free style
    that doesn't suck, I'll stick with this so I can run tests until I know what to
    do, in other words, this code will be rewritten over and over.
    
    Still untested code, but enough for writing regression tests.

--- lib/ssh_trans.ml
+++ lib/ssh_trans.ml
@@ -20,11 +20,59 @@ type conn_state =
   | NEW                         (* TCP established *)
   | VERXCHG                	(* We received and sent version_banner *)
 
-type conn = {
+type t = {
   state : conn_state;
-  buffer : Cstruct.t option;
+  buffer : Cstruct.t;
+  peer_version : string;
 }
 
-let conn_new () =
-  ({state = NEW; buffer = None},
+let add_buf t buf =
+  {state = t.state;
+   buffer = Cstruct.append t.buffer buf;
+   peer_version = t.peer_version}
+
+let make () =
+  ({state = NEW;
+    buffer = Cstruct.create 0;
+    peer_version = "unknown"},
    Cstruct.of_string version_banner)
+
+let find_some f = try Some (f ()) with Not_found -> None
+
+let process_new t =
+  (* SSH-2.0-billsSSH_3.6.3q3<CR><LF> *)
+  assert (t.state = NEW);
+  let s = Cstruct.to_string t.buffer in
+  let lines = Str.split_delim (Str.regexp "\r\n") s in
+  let line = find_some @@ fun () ->
+    List.find (fun line ->
+        String.length line >= 4 &&
+        String.sub line 0 4 = "SSH-")
+      lines
+  in
+  match line with
+  | None -> t
+  | Some line ->
+    if String.length line < 9 then
+      failwith "Version line is too short";
+    let tokens = Str.split_delim (Str.regexp "-") line in
+    if List.length tokens <> 3 then
+      failwith "Can't parse version line";
+    let version = List.nth tokens 1 in
+    let peer_version = List.nth tokens 2 in
+    if version <> "2.0" then
+      failwith ("Bad version " ^ version);
+    (* Now calculate how much we need to shift *)
+    let n = List.fold_left
+        (fun n line -> n + (String.length line))
+        (((List.length lines) - 1) * 2) (* account for \r\n *)
+        lines
+    in
+    {state = VERXCHG;
+     buffer = Cstruct.shift t.buffer n;
+     peer_version}
+
+let process t = match t.state with
+  | NEW -> process_new t  (* We're waiting for the banner *)
+  | _ -> failwith "todo"
+  
