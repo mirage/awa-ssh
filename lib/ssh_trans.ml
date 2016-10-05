1174d9e049eaaf43e02007a218ffba5b361df939
Christiano Haesbaert <haesbaert@haesbaert.org>
2016-10-05 18:40:56 +0200

    Clear comment.

--- lib/ssh_trans.ml
+++ lib/ssh_trans.ml
@@ -125,21 +125,6 @@ type kex_pkt = {
   first_kex_packet_follows : bool
 }
 
-      (* byte         SSH_MSG_KEXINIT *)
-      (* byte[16]     cookie (random bytes) *)
-      (* name-list    kex_algorithms *)
-      (* name-list    server_host_key_algorithms *)
-      (* name-list    encryption_algorithms_client_to_server *)
-      (* name-list    encryption_algorithms_server_to_client *)
-      (* name-list    mac_algorithms_client_to_server *)
-      (* name-list    mac_algorithms_server_to_client *)
-      (* name-list    compression_algorithms_client_to_server *)
-      (* name-list    compression_algorithms_server_to_client *)
-      (* name-list    languages_client_to_server *)
-      (* name-list    languages_server_to_client *)
-      (* boolean      first_kex_packet_follows *)
-      (* uint32       0 (reserved for future extension) *)
-
 (* Parse a name list as in RFC4251 5. *)
 let buf_of_namelist nl =
   let s = String.concat "," nl in
