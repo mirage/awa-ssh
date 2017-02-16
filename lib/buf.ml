8fed39a35e9db7dd86618e4b68e50e0aa1db9089
Christiano Haesbaert <haesbaert@haesbaert.org>
2017-02-16 02:18:40 +0100

    Add buf.ml
    The idea is to have a more efficient and less error prone Cstruct.t.
    
    Basically, we create Buf.t with 256 bytes, and the buffer automagically grow as
    we add stuff. The nice thing is that it allows us to have this idiom:
    
         add_id SSH_MSG_DEBUG (create ()) |>
          add_bool always_display |>
          add_string message |>
          add_string lang
    
    Before it would be something like
    
    let a = encode_bool always_display in
    let b = encode_string message in
    let c = encode_string lang in
    
    Cstruct.concat [a;b;c]
    
    Each encode would be one alloc, then we copy all the data to each buffer, and
    then later we recopy them while concatenating, that's ETOOMANYCOPIES.
    
    The negative point is that encoding uses Buf.t while decoding uses Cstruct.t.

---
+++ lib/buf.ml
@@ -0,0 +1,93 @@
+(*
+ * Copyright (c) 2017 Christiano F. Haesbaert <haesbaert@haesbaert.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+type t = {
+  tlen : int;
+  coff : int;
+  cbuf : Cstruct.t;
+}
+
+let chunk_size = 256
+
+let create () =
+  let tlen = chunk_size in
+  let cbuf = Cstruct.create tlen in
+  { tlen; coff = 0; cbuf }
+
+let to_cstruct t =
+  Cstruct.set_len t.cbuf t.coff
+
+let left t = t.tlen - t.coff
+
+let grow len t =
+  let tlen = t.tlen + len in
+  let cbuf = Cstruct.append t.cbuf (Cstruct.create len) in
+  { t with tlen; cbuf }
+
+let guard_space len t =
+  if (left t) >= len then t else grow (max len chunk_size) t
+
+let shift n t = { t with coff = t.coff + n }
+
+let add_uint8 b t =
+  let t = guard_space 1 t in
+  Cstruct.set_uint8 t.cbuf t.coff b;
+  shift 1 t
+
+let add_bool b t =
+  let x = if b then 1 else 0 in
+  add_uint8 x t
+
+let add_uint32 w t =
+  let t = guard_space 4 t in
+  Cstruct.BE.set_uint32 t.cbuf t.coff w;
+  shift 4 t
+
+let add_string s t =
+  let len = String.length s in
+  let t = add_uint32 (Int32.of_int len) t in
+  let t = guard_space len t in
+  Cstruct.blit_from_string s 0 t.cbuf t.coff len;
+  shift len t
+
+let add_cstring s t =
+  let len = Cstruct.len s in
+  let t = add_uint32 (Int32.of_int len) t in
+  let t = guard_space len t in
+  Cstruct.blit s 0 t.cbuf t.coff len;
+  shift len t
+
+let add_raw buf t =
+  let len = Cstruct.len buf in
+  let t = guard_space len t in
+  Cstruct.blit buf 0 t.cbuf t.coff len;
+  shift len t
+
+let add_nl nl t =
+  add_string (String.concat "," nl) t
+
+let add_mpint mpint t =
+  let mpbuf = Nocrypto.Numeric.Z.to_cstruct_be mpint in
+  let mplen = Cstruct.len mpbuf in
+  let t =
+    if mplen > 0 &&
+       ((Cstruct.get_uint8 mpbuf 0) land 0x80) <> 0 then
+      add_uint32 (Int32.of_int (succ mplen)) t |>
+      add_uint8 0
+    else
+      add_uint32 (Int32.of_int mplen) t
+  in
+  add_raw mpbuf t
