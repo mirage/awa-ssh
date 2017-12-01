a7c50c05965b88c533086e93a815864f6f154603
Christiano Haesbaert <haesbaert@haesbaert.org>
2017-12-01 17:05:12 +0000

    Better function names.

--- test/unix_server.ml
+++ test/unix_server.ml
@@ -40,31 +40,31 @@ let write_cstruct fd buf =
   let n = Unix.write fd bytes 0 len in
   assert (n > 0)
 
-let rec main_loop t fd =
+let rec serve t fd =
   let open Server in
   Engine.poll t >>= fun (t, poll_result) ->
   match poll_result with
   | Engine.No_input ->
-    Engine.input_buf t (read_cstruct fd) >>= fun t -> main_loop t fd
-  | Engine.Output buf -> write_cstruct fd buf; main_loop t fd
+    Engine.input_buf t (read_cstruct fd) >>= fun t -> serve t fd
+  | Engine.Output buf -> write_cstruct fd buf; serve t fd
   | Engine.Disconnected s -> ok (printf "Disconnected: %s\n%!" s)
   | Engine.Channel_eof c -> ok (printf "Got EOF\n%!")
   | Engine.Channel_data (id, data) ->
     (* XXX just send back, assume it is echo *)
-    Engine.send_channel_data t id data >>= fun t -> main_loop t fd
+    Engine.send_channel_data t id data >>= fun t -> serve t fd
   | Engine.Channel_exec (id, cmd) -> match cmd with
-    | "suicide" -> Engine.disconnect t >>= fun t -> main_loop t fd
+    | "suicide" -> Engine.disconnect t >>= fun t -> serve t fd
     | "ping" ->
       Engine.send_channel_data t id "pong\n" >>= fun t ->
       Engine.disconnect t >>= fun t ->
       printf "sent pong\n%!";
-      main_loop t fd
-    | "echo" -> main_loop t fd
+      serve t fd
+    | "echo" -> serve t fd
     | unknown ->
       let m = sprintf "Unknown command %s\n%!" cmd in
       Engine.send_channel_data t id m >>= fun t ->
       printf "%s\n%!" m;
-      Engine.disconnect t >>= fun t -> main_loop t fd
+      Engine.disconnect t >>= fun t -> serve t fd
 
 let user_db =
   (* User foo auths by passoword *)
@@ -82,7 +82,7 @@ let rec wait_connection rsa listen_fd server_port =
   let client_fd, _ = Unix.(accept listen_fd) in
   printf "Client connected !\n%!";
   let t = Server.make rsa user_db in
-  let () = match main_loop t client_fd with
+  let () = match serve t client_fd with
     | Ok _ -> printf "Client finished\n%!"
     | Error e -> printf "error: %s\n%!" e
   in
